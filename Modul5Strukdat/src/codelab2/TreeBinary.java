8308f9e609522080662c50a7742d9fe2d983056a
lailasyarifaa <lailatus.syarifah15@gmail.com>
2024-05-17 15:49:09 +0700

    codelab modul5

---
+++ Modul5Strukdat/src/codelab2/TreeBinary.java
@@ -0,0 +1,55 @@
+package codelab2;
+
+public class TreeBinary {
+    public Node root;
+    public TreeBinary(){
+        root = null;
+    }
+
+    // Fungsi untuk menambahkan node secara manual ke tree
+    public void addRoot(int data){
+        root = new Node(data);
+    }
+
+    public void inOrder(Node node){
+        if (node != null) {
+            inOrder(node.left);
+            System.out.println(node.data + " ");
+            inOrder(node.right);
+        }
+    }
+
+    public void preOrder(Node node){
+        if (node != null) {
+            System.out.println(node.data + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    public void postOrder(Node node){
+        if (node != null){
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.println(node.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeBinary tree = new TreeBinary();
+
+        tree.addRoot(20);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(25);
+        tree.root.left.left = new Node(37);
+        tree.root.left.right = new Node(12);
+        tree.root.right.right = new Node(16);
+
+        System.out.println("\nPre Order: ");
+        tree.preOrder(tree.root);
+        System.out.println("\nIn Order: ");
+        tree.inOrder(tree.root);
+        System.out.println("\nPost Order: ");
+        tree.postOrder(tree.root);
+    }
+}
